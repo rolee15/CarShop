1fece18192e08c766d6010f7465dc9591d2b1fd7
rolee15 <rolee15@gmail.com>
2024-07-22 14:19:37 +0200

    EF dependencies added and first models created

---
+++ CarShop.Data/Brand.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarShop.Data;
+
+public class Brand
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public string Name { get; set; } = default!;
+
+    // Navigational property
+    public ICollection<Car> Cars { get; set; }
+
+    public Brand()
+    {
+        Cars = new HashSet<Car>();
+    }
+}
---
+++ CarShop.Data/Car.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarShop.Data;
+
+public class Car
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public string Model { get; set; } = default!;
+
+    public int? Price { get; set; }
+
+    // Navigational property
+    public Brand Brand { get; set; } = default!;
+}
